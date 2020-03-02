28f28031a5502eb290716504a82d4b388a9bc29a
Moatez <47000103+MoatezNG@users.noreply.github.com>
2020-03-02 20:55:36 +0200

    User Jwt

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const userRouter = require('./routers/user')
+const port = process.env.PORT
+require('./db/db')
+
+const app = express()
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
+
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
